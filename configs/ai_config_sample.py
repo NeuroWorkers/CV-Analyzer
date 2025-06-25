3e1ee0cf346293308a5922f96719b68206cf5b84
Sv163Sam <Tankruuus@gmail.com>
2025-06-25 13:07:59 +0500

    ref configs

--- configs/ai_config_sample.py
+++ configs/ai_config_sample.py
@@ -1,13 +1,6 @@
-import os
 import openai
-
-BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '../'))
+from calculated_paths import *
 
 openai.api_key = ""
-
-messages_json = os.path.join(BASE_DIR, "tg_fetcher", "downloaded_text", "messages.json")
-relevant_messages_json = os.path.join(BASE_DIR, "database", "messages", "cv.json")
-relevant_messages_dir = os.path.join(BASE_DIR, "database", "messages")
-relevant_media = os.path.join(BASE_DIR, "database", "media")
 
 max_processing_message_count = 100
--- configs/telegram_config_sample.py
+++ configs/telegram_config_sample.py
@@ -1,17 +1,9 @@
-import os
-
-BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '../'))
+from calculated_paths import *
 
 API_ID = 0
 API_HASH = ''
 SESSION_STRING = ''
 
 group_username = 'https://t.me/horizontal_network'
-
-output_filename = os.path.join(BASE_DIR, "tg_fetcher", "downloaded_text", "messages.json")
-media_dir_parth = os.path.join(BASE_DIR, "tg_fetcher", "downloaded_media")
-output_dir = os.path.join(BASE_DIR, "tg_fetcher", "downloaded_text")
-last_dump_file = os.path.join(BASE_DIR, "tg_fetcher", "last_dump", "last_dump_date.txt")
-last_dump_dir = os.path.join(BASE_DIR, "tg_fetcher", "last_dump")
 
 specific_topic_id = 1275
