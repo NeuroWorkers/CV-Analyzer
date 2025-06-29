69b7b5786ffe3341b64b4361eb821ebf95571e04
Santas7 <andreypanyshkin03@gmail.com>
2025-06-29 17:32:40 +0400

    upd

--- ui/frontend/src/core/utils/extractFunctions.ts
+++ ui/frontend/src/core/utils/extractFunctions.ts
@@ -1,6 +1,6 @@
 export const extractUsername = (input: string): string => {
   const match = input.match(/^@(\S+)/);
-  return match ? match[1] : '';
+  return match ? `@${match[1]}` : '';
 }
 
 export const extractFullName = (input: string): string => {
