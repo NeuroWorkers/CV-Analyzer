f0dfe5a57ecaf91e9a44b6b785b025c7b9e1d068
aidev2075 <aidev2075@github.com>
2025-07-02 23:40:58 +0400

    cfgs from nodes:   cv-a-3-cfg--nw-013/*;cv-a-4-cfg--nw-013/

---
+++ deploy/cv-a-3-cfg--nw-013/config.sh
@@ -0,0 +1,2 @@
+export HTTP_SERVER_PORT="19103"
+export VENV_DIR="../venv"
---
+++ deploy/cv-a-4-cfg--nw-013/ai_config_sample.py
@@ -0,0 +1,8 @@
+import openai
+
+openai.api_key = ""
+openrouter_api_key = ""
+
+max_processing_message_count = 100
+
+MODEL_NAME = "sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2"
