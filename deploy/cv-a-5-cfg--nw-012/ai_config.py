5a6c224197721f38487a610341a09abbc29cbab6
aidev2075 <neuroworkers@ya.ru>
2025-07-06 16:15:27 +0400

    modified:   bin/;deploy/cv-a-5-cfg--nw-012/ai_config.py

---
+++ deploy/cv-a-5-cfg--nw-012/ai_config.py
@@ -0,0 +1,12 @@
+import openai
+
+openai.api_key = ""
+openrouter_api_key = ""
+
+max_processing_message_count = 100
+
+faiss_model = "sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2"
+
+openai_model = "google/gemini-2.5-flash"
+
+db_conn_name="database_3183"
