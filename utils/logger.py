507905b63c130c71f9227b66dc92601aeda0cd53
the80hz <dnl.vlgv@mail.ru>
2025-07-07 19:02:31 +0400

    Улучшена настройка логирования: добавлены параметры уровня, консоли и ротации файлового хендлера

--- utils/logger.py
+++ utils/logger.py
@@ -1,23 +1,42 @@
 import logging
 import os
+from logging.handlers import RotatingFileHandler
 
 
 class MyLogger:
-    def __init__(self, name):
+    def __init__(self, name, level=logging.INFO, console=True, max_bytes=10*1024*1024, backup_count=5):
         self.logger = logging.getLogger(name)
         if not self.logger.hasHandlers():
-            self.setup_logger(name)
+            self.setup_logger(name, level, console, max_bytes, backup_count)
 
-    def setup_logger(self, name):
+    def setup_logger(self, name, level, console, max_bytes, backup_count):
         log_file_name = f"logs/{name}_log.txt"
         os.makedirs('logs', exist_ok=True)
 
-        handler = logging.FileHandler(log_file_name)
-        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-        handler.setFormatter(formatter)
+        # Настройка уровня логирования
+        self.logger.setLevel(level)
 
-        self.logger.setLevel(logging.INFO)
-        self.logger.addHandler(handler)
+        # Форматтер
+        formatter = logging.Formatter(
+            '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+            datefmt='%Y-%m-%d %H:%M:%S'
+        )
+
+        # Файловый хандлер с ротацией
+        file_handler = RotatingFileHandler(
+            log_file_name, 
+            maxBytes=max_bytes, 
+            backupCount=backup_count,
+            encoding='utf-8'
+        )
+        file_handler.setFormatter(formatter)
+        self.logger.addHandler(file_handler)
+
+        # Консольный хандлер (опционально)
+        if console:
+            console_handler = logging.StreamHandler()
+            console_handler.setFormatter(formatter)
+            self.logger.addHandler(console_handler)
 
     def info(self, message):
         self.logger.info(message)
