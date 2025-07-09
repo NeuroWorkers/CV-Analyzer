a60e896edce415615170cd28be9f7600a6760f63
aidev2075 <aidev2075@github.com>
2025-07-09 13:59:59 +0400

    new sh configs 	modified:   bin/app_server;bin/app_start_nohup;bin/begin.sh

--- bin/begin.sh
+++ bin/begin.sh
@@ -6,10 +6,12 @@ if [ -f configs/config.sh ]; then
 source configs/config.sh
 fi
 
-#source configs/server_config_sample.py
-#if [ -f configs/server_config.py ]; then
-#source configs/server_config.py
-#fi
+./utils/prep_to_cfg.py configs/server_config_sample.py configs/server_config_sample.sh
+source configs/server_config_sample.sh
+if [ -f configs/server_config.py ]; then
+./utils/prep_to_cfg.py configs/server_config.py configs/server_config.sh
+source configs/server_config.sh
+fi
 
 
 if [ "x$VIRTUAL_ENV" == "x" ]; then
