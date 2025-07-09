82a8079a78125673c9913908c7255266c47308f9
aidev2075 <aidev2075@nw>
2025-07-09 12:35:45 +0400

    ../bin/app_start_systemd;  ../bin/begin.sh;   config.sh.sample

--- bin/begin.sh
+++ bin/begin.sh
@@ -1,11 +1,16 @@
 
 export PYTHONPATH=$(pwd):$PYTHONPATH
 
+source configs/config.sh.sample
 if [ -f configs/config.sh ]; then
 source configs/config.sh
-else
-source configs/config.sh.sample
 fi
+
+#source configs/server_config_sample.py
+#if [ -f configs/server_config.py ]; then
+#source configs/server_config.py
+#fi
+
 
 if [ "x$VIRTUAL_ENV" == "x" ]; then
 source ${VENV_DIR}/bin/activate
