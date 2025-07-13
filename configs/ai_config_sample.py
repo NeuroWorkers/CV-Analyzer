bb8e30baeb933481ec9b83a609263f35d591f780
Sv163Sam <Tankruuus@gmail.com>
2025-07-13 19:22:06 +0500

    UNVERSION MODEL TEST

--- configs/ai_config_sample.py
+++ configs/ai_config_sample.py
@@ -1,6 +1,6 @@
 max_processing_message_count = 100
 
-faiss_model = "sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2"
+faiss_model = "intfloat/multilingual-e5-base"
 
 sort_model = "openai/gpt-4"
 analyze_query_model = "openai/gpt-4"
@@ -8,7 +8,7 @@ highlight_model = "google/gemini-2.5-flash"
 filter_trash_model = "google/gemini-2.5-flash"
 
 faiss_deep = 100
-chunk_threshold = 0.81
+chunk_threshold = 0.91
 N_LIST = 100
 EMBEDDING_DIM = 384
 N_PROBE = 20
