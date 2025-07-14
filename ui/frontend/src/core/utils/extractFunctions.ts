71f9a5dc2b7129df57db3cb3a0df6e75337efc8e
the80hz <dnl.vlgv@mail.ru>
2025-07-14 15:44:29 +0400

    убрал @ из ссылки

--- ui/frontend/src/core/utils/extractFunctions.ts
+++ ui/frontend/src/core/utils/extractFunctions.ts
@@ -3,6 +3,11 @@ export const extractUsername = (input: string): string => {
   return match ? `@${match[1]}` : '';
 }
 
+export const extractUsernameWithoutAt = (input: string): string => {
+  const match = input.match(/^@(\S+)/);
+  return match ? match[1] : '';
+}
+
 export const extractFullName = (input: string): string => {
   const withoutUsername = input.replace(/^@\S+\s*/, '');
   return withoutUsername.trim();
