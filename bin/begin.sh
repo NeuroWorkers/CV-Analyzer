11a3f6e8d3a8803400290979896dacc9766800f2
aidev2075 <aidev2075@nw>
2025-07-16 15:43:14 +0400

    add conda support in scripts and cfgs

--- bin/begin.sh
+++ bin/begin.sh
@@ -12,7 +12,13 @@ if [ -f configs/server_config.py ]; then
 ./utils/prep_to_cfg.py configs/server_config.py configs/server_config.sh
 source configs/server_config.sh
 fi
-
+if [ "x$VIRTUAL_ENV" == "x" ]; then
+if [ ! "x$VENV_CONDA_ID" == "x" ]; then
+CONDA_BASE=$(conda info --base); source $CONDA_BASE/etc/profile.d/conda.sh
+conda activate $VENV_CONDA_ID
+echo "VENV_CONDA_ID=$VENV_CONDA_ID"
+fi
+fi
 
 if [ "x$VIRTUAL_ENV" == "x" ]; then
 source ${VENV_DIR}/bin/activate
